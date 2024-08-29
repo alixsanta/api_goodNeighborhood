3386b266f998a1dbf18eb0c05a3783825550bb15
Alix Allegre <alixallegre31@gmail.com>
2024-08-29 11:28:57 +0200

    feat: Update AuthController file

--- api/src/Controller/AuthController.php
+++ api/src/Controller/AuthController.php
@@ -3,54 +3,60 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use App\Service\PasswordService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\User\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AuthController extends AbstractController
-{   
+{
+    private $passwordService;
+    private $jwtManager;
+    private $userAuthenticator;
+    private $entityManager;
+
+    public function __construct(
+        PasswordService $passwordService,
+        JWTTokenManagerInterface $jwtManager,
+        UserAuthenticatorInterface $userAuthenticator,
+        EntityManagerInterface $entityManager
+    ) {
+        $this->passwordService = $passwordService;
+        $this->jwtManager = $jwtManager;
+        $this->userAuthenticator = $userAuthenticator;
+        $this->entityManager = $entityManager;
+    }
+
     // User authentification with JWT
-    #[Route('/api/login_check', name: 'api_login', methods: ['POST'])]
-    public function login(
-        Request $request, 
-        EntityManagerInterface $entityManager, 
-        UserPasswordHasherInterface $passwordHasher, 
-        JWTTokenManagerInterface $jwtManager
-    ): JsonResponse {
-        // Retrieves the contents of the query
+    #[Route('/api/login', name: 'login', methods: ['POST'])]
+    public function login(Request $request): JsonResponse
+    {
+        // Retrieves query data
         $requestData = json_decode($request->getContent(), true);
+
         $email = $requestData['mailUser'] ?? null;
         $password = $requestData['password'] ?? null;
 
-        // Check required fields
+        // Checks required fields
         if (!$email || !$password) {
-            return new JsonResponse(['error' => 'l\'E-mail et mot de passe est requis'], 400);
+            return new JsonResponse(['error' => 'L\email et le mot de passe sont nécessaires'], 400);
         }
 
-        // Surch user by e-mail
-        $user = $entityManager->getRepository(User::class)->findOneBy(['mailUser' => $email]);
+        // Search user by email
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['mailUser' => $email]);
 
-        // Checks if user exists
-        if (!$user) {
-            return new JsonResponse(['error' => 'L\'utilisateur n\'existe pas'], 404);
-        }
-
-        // Checks if password is correct
-        if (!$passwordHasher->isPasswordValid($user, $password)) {
-            return new JsonResponse(['error' => 'Le mot de passe est incorrect'], 401);
+        if (!$user || !$this->passwordService->isPasswordValid($user, $password)) {
+            return new JsonResponse(['error' => 'Informations d\'identification invalides'], 401);
         }
 
         // Generates JWT token
-        $token = $jwtManager->create($user);
-
-        // JSON response with token
-        return new JsonResponse([
-            'token' => $token,
-            'message' => 'Connexion réussie',
-        ], 200);
+        $token = $this->jwtManager->create($user);
+        // Json response with token
+        return new JsonResponse(['token' => $token, 'message' => 'Connexion réussie'], 200);
     }
 }
